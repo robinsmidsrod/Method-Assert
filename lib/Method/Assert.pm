7be63f06f3600e79af4eb4d215aeb72c8c673ab3
Robin Smidsrød <robin@smidsrod.no>
2010-07-29 15:37:19 +0200

    Added basic documentation

--- lib/Method/Assert.pm
+++ lib/Method/Assert.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Method::Assert;
 
-# ABSTRACT: Ensure instance or class methods are called properly
+# ABSTRACT: Ensure instance and class methods are called properly
 
 use Scalar::Util qw(blessed);
 use Carp qw(confess);
@@ -58,6 +58,99 @@ sub import {
 }
 
 =pod
+
+=head1 SYNOPSIS
+
+    package MyClass;
+
+    use Method::Assert;
+    use Carp qw(confess);
+
+    sub new {
+        my ($class, @args) = &class_method;
+        my $self = {};
+        bless $self, $class;
+        $self->_init(@args);
+        return $self;
+    }
+
+    sub _init {
+        my ($self, @args) = @_; # Perl Critic prefers it this way
+        &instance_method;       # still works
+        ...
+    }
+
+    sub get_output_filename {
+        &instance_method;
+        return shift->{'output_filename'};
+    }
+
+    sub set_output_filename {
+        my $self = &instance_method;
+        confess("No parameter specified") unless @_;
+        confess("File already exists") if -e $_[0];
+        $self->{'output_filename'} = $_[0];
+        return $self;
+    }
+
+
+=head1 DESCRIPTION
+
+This module will export the two functions named below into the namespace of
+the package using it. These two functions are useful to do typical checks at
+the start of functions that are supposed to be either class or instance
+methods.
+
+B<Always remember to call these functions as C<&class_method> and
+C<&instance_method>, or else they will not work properly!>
+
+If you call them as C<class_method()> or C<instance_method()> a new version
+of @_ will be initialized, and manipulation of @_ will not work properly.
+
+
+=func class_method
+
+Use this function to check that the sub is called as a class method. If the
+sub is called as a function, or as an instance method this function will
+die.
+
+If called in scalar context, will shift of the first argument of the @_
+array and return that value. If called in list or void context it will not
+change @_.
+
+
+=func instance_method
+
+Use this function to check that the sub is called as an instance method. If
+the sub is called as a function, or as a class method this function will
+die.
+
+If called in scalar context, will shift of the first argument of the @_
+array and return that value. If called in list or void context it will not
+change @_.
+
+
+=head1 CAVEATS
+
+The two functions can NOT be called by the fully qualified method name
+because they are generated as closures in the calling package's namespace
+during import(). Writing C<use Method::Assert ()> will cause import() not to
+be executed, which doesn't not make sense. See the section on I<import> in
+C<perldoc -f use> for more information.
+
+
+=head1 SEMANTIC VERSIONING
+
+This module uses semantic versioning concepts from L<http://semver.org/>.
+
+
+=head1 SEE ALSO
+
+=for :list
+* L<Method::Signatures>
+* L<Devel::Declare>
+* L<MooseX::Method::Signatures>
+* L<MooseX::Declare>
 
 =cut
 
